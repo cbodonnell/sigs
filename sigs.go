51fb9f173ff084e09dac57d91b380f4983930cca
cbodonnell <cbodonnell@comcast.net>
2021-08-07 10:14:47 -0400

    read/parse priv/pub

--- sigs.go
+++ sigs.go
@@ -19,9 +19,7 @@ type SignedString struct {
 }
 
 func main() {
-	// The GenerateKey method takes in a reader that returns random bits, and
-	// the number of bits
-	privateKey, err := ParseRSA("/users/craig/crypto/private.pem")
+	privateKey, err := ParseRSAPrivateKey("/users/craig/crypto/private.pem")
 	if err != nil {
 		panic(err)
 	}
@@ -31,101 +29,108 @@ func main() {
 		panic(err)
 	}
 
-	// To verify the signature, we provide the public key, the hashing algorithm
-	// the hash sum of our message and the signature we generated previously
-	// there is an optional "options" parameter which can omit for now
-	err = CheckSignature(privateKey, signedString)
+	err = CheckSignature(&privateKey.PublicKey, signedString)
 	if err != nil {
 		panic(err)
 	}
-	// If we don't get any error from the `VerifyPSS` method, that means our
-	// signature is valid
+
 	fmt.Println("signature verified")
 }
 
-func ParseRSA(rsaPrivateKeyLocation string) (*rsa.PrivateKey, error) {
-	priv, err := ioutil.ReadFile(rsaPrivateKeyLocation)
-	if err != nil {
-		return nil, err
-	}
-
-	privPem, _ := pem.Decode(priv)
+func ReadPrivateKey(b []byte) (*rsa.PrivateKey, error) {
+	privPem, _ := pem.Decode(b)
 	if privPem.Type != "RSA PRIVATE KEY" {
 		return nil, errors.New("RSA private key is of the wrong type")
 	}
 
 	var parsedKey interface{}
-	if parsedKey, err = x509.ParsePKCS1PrivateKey(privPem.Bytes); err != nil {
+	parsedKey, err := x509.ParsePKCS1PrivateKey(privPem.Bytes)
+	if err != nil {
 		if parsedKey, err = x509.ParsePKCS8PrivateKey(privPem.Bytes); err != nil {
 			return nil, err
 		}
 	}
 
-	var privateKey *rsa.PrivateKey
-	var ok bool
-	privateKey, ok = parsedKey.(*rsa.PrivateKey)
+	privateKey, ok := parsedKey.(*rsa.PrivateKey)
 	if !ok {
-		return nil, err
+		return nil, errors.New("unable to parse RSA private key")
 	}
-
-	// pub, err := ioutil.ReadFile(rsaPublicKeyLocation)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// pubPem, _ := pem.Decode(pub)
-	// if pubPem == nil {
-	// 	return nil, errors.New("rsa public key not in pem format")
-	// }
-	// if pubPem.Type != "PUBLIC KEY" {
-	// 	return nil, fmt.Errorf("RSA public key is of the wrong type %s", pubPem.Type)
-	// }
-
-	// if parsedKey, err = x509.ParsePKIXPublicKey(pubPem.Bytes); err != nil {
-	// 	return nil, err
-	// }
-
-	// var pubKey *rsa.PublicKey
-	// if pubKey, ok = parsedKey.(*rsa.PublicKey); !ok {
-	// 	return nil, errors.New("unable to parse RSA public key")
-	// }
-
-	// privateKey.PublicKey = *pubKey
 
 	return privateKey, nil
 }
 
-func SignString(privateKey *rsa.PrivateKey, s string) (SignedString, error) {
+func ParseRSAPrivateKey(rsaPrivateKeyLocation string) (*rsa.PrivateKey, error) {
+	priv, err := ioutil.ReadFile(rsaPrivateKeyLocation)
+	if err != nil {
+		return nil, err
+	}
 
+	return ReadPrivateKey(priv)
+}
+
+func ReadPublicKey(b []byte) (*rsa.PublicKey, error) {
+	pubPem, _ := pem.Decode(b)
+	if pubPem == nil {
+		return nil, errors.New("rsa public key not in pem format")
+	}
+	if pubPem.Type != "PUBLIC KEY" {
+		return nil, fmt.Errorf("RSA public key is of the wrong type %s", pubPem.Type)
+	}
+
+	parsedKey, err := x509.ParsePKIXPublicKey(pubPem.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	var pubKey *rsa.PublicKey
+	pubKey, ok := parsedKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("unable to parse RSA public key")
+	}
+
+	return pubKey, nil
+}
+
+func ParseRSAPublicKey(rsaPublicKeyLocation string) (*rsa.PublicKey, error) {
+	pub, err := ioutil.ReadFile(rsaPublicKeyLocation)
+	if err != nil {
+		return nil, err
+	}
+
+	return ReadPublicKey(pub)
+}
+
+func SignString(privateKey *rsa.PrivateKey, s string) (SignedString, error) {
 	var signedString SignedString
 
-	b := []byte(s)
-
-	// Before signing, we need to hash our message
-	// The hash is what we actually sign
-	msgHash := sha256.New()
-	_, err := msgHash.Write(b)
+	hashSum, err := HashSum(s)
 	if err != nil {
 		return signedString, err
 	}
-	signedString.HashSum = msgHash.Sum(nil)
+	signedString.HashSum = hashSum
 
-	// In order to generate the signature, we provide a random number generator,
-	// our private key, the hashing algorithm that we used, and the hash sum
-	// of our message
 	signature, err := rsa.SignPSS(rand.Reader, privateKey, crypto.SHA256, signedString.HashSum, nil)
 	if err != nil {
 		return signedString, err
 	}
-
 	signedString.Signature = signature
 
 	return signedString, nil
 
 }
 
-func CheckSignature(privateKey *rsa.PrivateKey, signedString SignedString) error {
-	err := rsa.VerifyPSS(&privateKey.PublicKey, crypto.SHA256, signedString.HashSum, signedString.Signature, nil)
+func HashSum(s string) ([]byte, error) {
+	b := []byte(s)
+	msgHash := sha256.New()
+	_, err := msgHash.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	return msgHash.Sum(nil), nil
+}
+
+func CheckSignature(publicKey *rsa.PublicKey, signedString SignedString) error {
+	err := rsa.VerifyPSS(publicKey, crypto.SHA256, signedString.HashSum, signedString.Signature, nil)
 	if err != nil {
 		return err
 	}
